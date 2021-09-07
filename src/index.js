ef0fdbf4b212898caae108e4e47da7a535267f7d
Raven <ravenkoehler@gmail.com>
2021-09-07 12:58:47 -0400

    commit tree 23b7f6a6a4dd31648c028e25e7b0bdf484a5fbfc

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  fecthToys();
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -13,3 +14,35 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function fetchToys() {
+  fetch("http://localhost:3000/toys")
+  .then(rsp => resp.json())
+  .then(function(toys) {
+    const toyDiv = document.getElementById("toy-collection");
+    for (const toy of toys) {
+      const card = createCard();
+      updateCard(card, toy);
+      toyDiv.appendChild(card);
+    }
+  })
+}
+
+function createCard() {
+  const card = document.createElement("div");
+  card.className = "card";
+  const h2 = document.createElement("h2");
+  const img = document.createElement("img");
+  const p = document.createElement("p");
+  const button = document.createElement("button");
+  button.className = "like-btn";
+  button.innerText = "Like";
+  card.append(h2, img, p, button);
+  return card;
+}
+
+function updateCard(card, toy) {
+  card.querySelector("h2").innerText = toy.name;
+  card.querySelector("img").src = toy.image;
+  card.querySelector("p").innerText = toy.likes;
+}
